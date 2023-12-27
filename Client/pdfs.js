5f2dcb52090ce02931a279be0a2d6d2db6d76515
Elinuuur <151919593+Elinuuur@users.noreply.github.com>
2023-12-27 09:08:26 +0100

    server update

---
+++ Client/pdfs.js
@@ -0,0 +1,35 @@
+
+async function search() {
+
+    let searchTerm = document.forms.searchForm.term.value;
+   
+    let searchType = document.forms.searchForm.searchType.value;
+    console.log(searchType);
+   
+    document.forms.searchForm.term.value = '';
+   
+    let rawData = await fetch('/api/pdfs/' + searchTerm + '/' + searchType);
+    
+    let pdfs = await rawData.json();
+   
+    let html = `
+      <p>You searched for "${searchTerm}"...</p>
+      <p>Found ${pdf.length} PDF.</p>
+    `;
+    
+    for (let pdf of pdfs) {
+      let meta = pdf.description.common;
+      html += ` 
+        <section>
+          <h2>${meta.Title}</h2>
+          <p><b>Author:</b>${meta.Author}</p>
+          <p><b>Creator:</b>${meta.Creator}</p>
+            <a href="pdfs/${pdf.pdfsName}">Download the PDF</a>.
+        </section>
+      `;
+    }
+    s
+    let searchResultsElement = document.querySelector('.searchResults');
+    
+    searchResultsElement.innerHTML = html;
+  }
--- server.js
+++ server.js
@@ -23,26 +23,58 @@ async function query(sql, listOfValues) {
 }
 
 app.get('/api/music/:searchTerm/:searchType', async (request, response) => {
-
-  let searchTerm = request.params.searchTerm;
-
   let searchType = request.params.searchType;
-
   let sql = `
    SELECT * 
    FROM music
-   WHERE LOWER(metadata -> '$.common.${searchType}') LIKE LOWER (?)
+   WHERE LOWER(description -> '$.common.${searchType}') LIKE LOWER (?)
   `;
-
+  
   if (searchType == 'all') {
     sql = `
       SELECT *
       FROM music
-      WHERE LOWER(metadata) LIKE LOWER (?)
+      WHERE LOWER(description) LIKE LOWER (?)
     `;
   }
 
   let result = await query(sql, ['%' + searchTerm + '%']);
+
+  response.json(result);
+});
+
+app.get('/api/images/:searchTerm', async (request, response) => {
+  let searchTerm = request.params.searchTerm;
+  let result = await query(`
+    SELECT *
+    FROM images
+    WHERE LOWER (description) LIKE LOWER(?)
+  `, ['%' + searchTerm + '%']);
+ 
+  response.json(result);
+});
+
+app.get('/api/pdfs/:searchTerm', async (request, response) => {
+  let searchTerm = request.params.searchTerm;
+
+  let result = await query(`
+    SELECT *
+    FROM pdf
+    WHERE LOWER (description -> '$.info') LIKE LOWER(?)
+  `, ['%' + searchTerm + '%']);
+
+  response.json(result);
+});
+
+app.get('/api/ppts/:searchTerm', async (request, response) => {
+  
+  let searchTerm = request.params.searchTerm;
+
+  let result = await query(`
+    SELECT *
+    FROM ppts
+    WHERE LOWER(description)LIKE LOWER(?)
+  `, ['%' + searchTerm + '%']);
 
   response.json(result);
 });
